bc4dee2db16fdbd9a4c92788358c9e9be3615716
Vitor Pamplona <vitor@vitorpamplona.com>
2020-04-02 13:37:10 -0400

    Only asks permissions on Android

--- example/app/Entry.js
+++ example/app/Entry.js
@@ -26,17 +26,19 @@ import { PermissionsAndroid } from 'react-native';
 
 export async function requestLocationPermission() {
   try {
-    const granted = await PermissionsAndroid.request(
-      PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
-      {
-        'title': 'BLE Avertiser Example App',
-        'message': 'Example App access to your location '
+    if (Platform.OS === 'android') {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+        {
+          'title': 'BLE Avertiser Example App',
+          'message': 'Example App access to your location '
+        }
+      )
+      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+        console.log("You can use the location")
+      } else {
+        console.log("location permission denied")
       }
-    )
-    if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-      console.log("You can use the location")
-    } else {
-      console.log("location permission denied")
     }
 
     const blueoothActive = await BLEAdvertiserModule.getAdapterState().then(result => {
