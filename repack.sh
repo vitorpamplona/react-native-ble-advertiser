abc4938540324c8c24817d5dbc15d226d47950f0
Vitor Pamplona <vitor@vitorpamplona.com>
2020-04-08 10:39:05 -0400

    allowing repack and compile with options for ios.
    ./repack.sh android
    ./repack.sh ios

--- repack.sh
+++ repack.sh
@@ -1,5 +1,15 @@
 npm pack
 cd example
 npm i ../react-native-ble-advertiser-0.0.10.tgz
-npx react-native run-android
+
+if [ $1 = "android" ]
+then
+    npx react-native run-android
+else
+    cd ios
+    pod install
+    cd ..
+    npx react-native run-ios
+fi
+
 cd ..
