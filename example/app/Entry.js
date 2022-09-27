d4f066a5e27bc8604ce96ca882d11e6e041e7083
Vitor Pamplona <vitor@vitorpamplona.com>
2022-09-27 11:52:08 -0400

    Prettier

--- example/app/Entry.js
+++ example/app/Entry.js
@@ -1,31 +1,30 @@
-import React, {Component } from 'react';
+import React, {Component} from 'react';
 
 import {
   Alert,
   SafeAreaView,
   StyleSheet,
-  ScrollView,
   View,
   Text,
   TouchableOpacity,
   FlatList,
-  Platform
+  Platform,
 } from 'react-native';
 
-import { NativeEventEmitter, NativeModules } from 'react-native';
+import {NativeEventEmitter, NativeModules} from 'react-native';
 
 import update from 'immutability-helper';
 import BLEAdvertiser from 'react-native-ble-advertiser';
 import UUIDGenerator from 'react-native-uuid-generator';
-import { PermissionsAndroid } from 'react-native';
+import {PermissionsAndroid} from 'react-native';
 
 // Uses the Apple code to pick up iPhones
-const APPLE_ID = 0x4C;
+const APPLE_ID = 0x4c;
 const MANUF_DATA = [1, 0];
 // No scanner filters (finds all devices inc iPhone). Use UUID suffix to filter scans if using.
-const SCAN_MANUF_DATA = Platform.OS == 'android' ? null : MANUF_DATA;
+const SCAN_MANUF_DATA = Platform.OS === 'android' ? null : MANUF_DATA;
 
-BLEAdvertiser.setCompanyId(APPLE_ID); 
+BLEAdvertiser.setCompanyId(APPLE_ID);
 
 export async function requestLocationPermission() {
   try {
@@ -33,24 +32,26 @@ export async function requestLocationPermission() {
       const granted = await PermissionsAndroid.request(
         PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
         {
-          'title': 'BLE Avertiser Example App',
-          'message': 'Example App access to your location '
-        }
-      )
+          title: 'BLE Avertiser Example App',
+          message: 'Example App access to your location ',
+        },
+      );
       if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-        console.log('[Permissions]', 'Location Permission granted')
+        console.log('[Permissions]', 'Location Permission granted');
       } else {
-        console.log('[Permissions]', 'Location Permission denied')
+        console.log('[Permissions]', 'Location Permission denied');
       }
     }
 
-    const blueoothActive = await BLEAdvertiser.getAdapterState().then(result => {
-      console.log('[Bluetooth]', 'Bluetooth Status', result)
-      return result === "STATE_ON";
-    }).catch(error => { 
-      console.log('[Bluetooth]', 'Bluetooth Not Enabled')
-      return false;
-    });
+    const blueoothActive = await BLEAdvertiser.getAdapterState()
+      .then((result) => {
+        console.log('[Bluetooth]', 'Bluetooth Status', result);
+        return result === 'STATE_ON';
+      })
+      .catch((error) => {
+        console.log('[Bluetooth]', 'Bluetooth Not Enabled');
+        return false;
+      });
 
     if (!blueoothActive) {
       await Alert.alert(
@@ -67,168 +68,198 @@ export async function requestLocationPermission() {
             style: 'cancel',
           },
         ],
-      )
+      );
     }
   } catch (err) {
-    console.warn(err)
+    console.warn(err);
   }
 }
 
 class Entry extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            uuid:'',
-            isLogging: false,
-            devicesFound:[]
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      uuid: '',
+      isLogging: false,
+      devicesFound: [],
+    };
+  }
 
-    addDevice(_uuid, _name, _mac, _rssi, _date) {
-      const index = this.state.devicesFound.findIndex( ({ uuid }) => uuid == _uuid);
-      if (index<0) {
-        this.setState({
-          devicesFound: update(this.state.devicesFound, 
-            {$push: [{uuid:_uuid, name:_name, mac: _mac, rssi:_rssi, start:_date, end:_date}]}
-          )
-        });
-      } else {
-        this.setState({
-          devicesFound: update(this.state.devicesFound, 
-            {[index]: {end: {$set: _date}, rssi: {$set: _rssi || this.state.devicesFound[index].rssi }}}
-          )
-        });
-      }
-    }
-
-    componentDidMount(){
-      requestLocationPermission();
-      UUIDGenerator.getRandomUUID((newUid) => {
-        this.setState({
-          uuid: newUid.slice(0, -2) + '00'
-        });
+  addDevice(_uuid, _name, _mac, _rssi, _date) {
+    const index = this.state.devicesFound.findIndex(({uuid}) => uuid === _uuid);
+    if (index < 0) {
+      this.setState({
+        devicesFound: update(this.state.devicesFound, {
+          $push: [
+            {
+              uuid: _uuid,
+              name: _name,
+              mac: _mac,
+              rssi: _rssi,
+              start: _date,
+              end: _date,
+            },
+          ],
+        }),
+      });
+    } else {
+      this.setState({
+        devicesFound: update(this.state.devicesFound, {
+          [index]: {
+            end: {$set: _date},
+            rssi: {$set: _rssi || this.state.devicesFound[index].rssi},
+          },
+        }),
       });
     }
+  }
 
-    componentWillUnmount() {
-      if (this.state.isLogging)
-        this.stop();
+  componentDidMount() {
+    requestLocationPermission();
+    UUIDGenerator.getRandomUUID((newUid) => {
+      this.setState({
+        uuid: newUid.slice(0, -2) + '00',
+      });
+    });
+  }
+
+  componentWillUnmount() {
+    if (this.state.isLogging) {
+      this.stop();
     }
+  }
 
-    start() {
-      console.log(this.state.uuid, "Registering Listener");
-      const eventEmitter = new NativeEventEmitter(NativeModules.BLEAdvertiser);
+  start() {
+    console.log(this.state.uuid, 'Registering Listener');
+    const eventEmitter = new NativeEventEmitter(NativeModules.BLEAdvertiser);
 
-      this.onDeviceFound = eventEmitter.addListener('onDeviceFound', (event) => {
-        //console.log('onDeviceFound', event);
-        if (event.serviceUuids) {
-          for(let i=0; i< event.serviceUuids.length; i++){
-            if (event.serviceUuids[i] && event.serviceUuids[i].endsWith('00'))
-              this.addDevice(event.serviceUuids[i], event.deviceName, event.deviceAddress, event.rssi, new Date())   
+    this.onDeviceFound = eventEmitter.addListener('onDeviceFound', (event) => {
+      //console.log('onDeviceFound', event);
+      if (event.serviceUuids) {
+        for (let i = 0; i < event.serviceUuids.length; i++) {
+          if (event.serviceUuids[i] && event.serviceUuids[i].endsWith('00')) {
+            this.addDevice(
+              event.serviceUuids[i],
+              event.deviceName,
+              event.deviceAddress,
+              event.rssi,
+              new Date(),
+            );
           }
         }
-      });
+      }
+    });
 
-      console.log(this.state.uuid, "Starting Advertising");
-      BLEAdvertiser.broadcast(this.state.uuid, MANUF_DATA, {
-        advertiseMode: BLEAdvertiser.ADVERTISE_MODE_BALANCED, 
-        txPowerLevel: BLEAdvertiser.ADVERTISE_TX_POWER_MEDIUM, 
-        connectable: false, 
-        includeDeviceName: false, includeTxPowerLevel: false })
-        .then(sucess => console.log(this.state.uuid, "Adv Successful", sucess))
-        .catch(error => console.log(this.state.uuid, "Adv Error", error));
-      
-      console.log(this.state.uuid, "Starting Scanner");
-      BLEAdvertiser.scan(SCAN_MANUF_DATA, {scanMode: BLEAdvertiser.SCAN_MODE_LOW_LATENCY})
-        .then(sucess => console.log(this.state.uuid, "Scan Successful", sucess))
-        .catch(error => console.log(this.state.uuid, "Scan Error", error));
+    console.log(this.state.uuid, 'Starting Advertising');
+    BLEAdvertiser.broadcast(this.state.uuid, MANUF_DATA, {
+      advertiseMode: BLEAdvertiser.ADVERTISE_MODE_BALANCED,
+      txPowerLevel: BLEAdvertiser.ADVERTISE_TX_POWER_MEDIUM,
+      connectable: false,
+      includeDeviceName: false,
+      includeTxPowerLevel: false,
+    })
+      .then((sucess) => console.log(this.state.uuid, 'Adv Successful', sucess))
+      .catch((error) => console.log(this.state.uuid, 'Adv Error', error));
 
-      this.setState({
-        isLogging: true,
-      });
-    }
+    console.log(this.state.uuid, 'Starting Scanner');
+    BLEAdvertiser.scan(SCAN_MANUF_DATA, {
+      scanMode: BLEAdvertiser.SCAN_MODE_LOW_LATENCY,
+    })
+      .then((sucess) => console.log(this.state.uuid, 'Scan Successful', sucess))
+      .catch((error) => console.log(this.state.uuid, 'Scan Error', error));
 
-    stop(){
-      console.log(this.state.uuid, "Removing Listener");
-      this.onDeviceFound.remove();
-      delete this.onDeviceFound;
+    this.setState({
+      isLogging: true,
+    });
+  }
 
-      console.log(this.state.uuid, "Stopping Broadcast");
-      BLEAdvertiser.stopBroadcast()
-        .then(sucess => console.log(this.state.uuid, "Stop Broadcast Successful", sucess))
-        .catch(error => console.log(this.state.uuid, "Stop Broadcast Error", error));
+  stop() {
+    console.log(this.state.uuid, 'Removing Listener');
+    this.onDeviceFound.remove();
+    delete this.onDeviceFound;
 
-      console.log(this.state.uuid, "Stopping Scanning");
-      BLEAdvertiser.stopScan()
-        .then(sucess => console.log(this.state.uuid, "Stop Scan Successful", sucess) )
-        .catch(error => console.log(this.state.uuid, "Stop Scan Error", error) );
+    console.log(this.state.uuid, 'Stopping Broadcast');
+    BLEAdvertiser.stopBroadcast()
+      .then((sucess) => console.log(this.state.uuid, 'Stop Broadcast Successful', sucess))
+      .catch((error) => console.log(this.state.uuid, 'Stop Broadcast Error', error));
 
-      this.setState({
-        isLogging: false,
-      });
-    }
+    console.log(this.state.uuid, 'Stopping Scanning');
+    BLEAdvertiser.stopScan()
+      .then((sucess) => console.log(this.state.uuid, 'Stop Scan Successful', sucess))
+      .catch((error) => console.log(this.state.uuid, 'Stop Scan Error', error));
 
-    short(str) {
-      return (str.substring(0, 4) + " ... " + str.substring(str.length-4, str.length)).toUpperCase(); 
-    }
+    this.setState({
+      isLogging: false,
+    });
+  }
 
-    render() {
-      return (
-        <SafeAreaView>
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>BLE Advertiser Demo</Text>
-              <Text style={styles.sectionDescription}>Broadcasting: <Text style={styles.highlight}>{ this.short(this.state.uuid) }</Text></Text>
-            </View>
+  short(str) {
+    return (
+      str.substring(0, 4) +
+      ' ... ' +
+      str.substring(str.length - 4, str.length)
+    ).toUpperCase();
+  }
 
-            <View style={styles.sectionContainer}>
-              {this.state.isLogging ? (
+  render() {
+    return (
+      <SafeAreaView>
+        <View style={styles.body}>
+          <View style={styles.sectionContainer}>
+            <Text style={styles.sectionTitle}>BLE Advertiser Demo</Text>
+            <Text style={styles.sectionDescription}>
+              Broadcasting:{' '}
+              <Text style={styles.highlight}>
+                {this.short(this.state.uuid)}
+              </Text>
+            </Text>
+          </View>
+
+          <View style={styles.sectionContainer}>
+            {this.state.isLogging ? (
               <TouchableOpacity
                 onPress={() => this.stop()}
                 style={styles.stopLoggingButtonTouchable}>
-                <Text style={styles.stopLoggingButtonText}>
-                  Stop
-                </Text>
+                <Text style={styles.stopLoggingButtonText}>Stop</Text>
               </TouchableOpacity>
-                ) : (
+            ) : (
               <TouchableOpacity
                 onPress={() => this.start()}
                 style={styles.startLoggingButtonTouchable}>
-                <Text style={styles.startLoggingButtonText}>
-                  Start
-                </Text>
+                <Text style={styles.startLoggingButtonText}>Start</Text>
               </TouchableOpacity>
-              )}
-            </View>
-
-            <View style={styles.sectionContainerFlex}>
-              <Text style={styles.sectionTitle}>Devices Around</Text>
-              <FlatList
-                  data={ this.state.devicesFound }
-                  renderItem={({item}) => <Text style={styles.itemPastConnections}>{this.short(item.uuid)} {item.mac} {item.rssi}</Text>}
-                  keyExtractor={item => item.uuid}
-                  />
-            </View>
-
-            <View style={styles.sectionContainer}>
-              <TouchableOpacity
-                onPress={() => this.setState({ devicesFound: [] }) }
-                style={styles.startLoggingButtonTouchable}>
-                <Text style={styles.startLoggingButtonText}>
-                  Clear Devices
-                </Text>
-              </TouchableOpacity>
-            </View>
-
+            )}
           </View>
-        </SafeAreaView>
-      );
-    }
+
+          <View style={styles.sectionContainerFlex}>
+            <Text style={styles.sectionTitle}>Devices Around</Text>
+            <FlatList
+              data={this.state.devicesFound}
+              renderItem={({item}) => (
+                <Text style={styles.itemPastConnections}>
+                  {this.short(item.uuid)} {item.mac} {item.rssi}
+                </Text>
+              )}
+              keyExtractor={(item) => item.uuid}
+            />
+          </View>
+
+          <View style={styles.sectionContainer}>
+            <TouchableOpacity
+              onPress={() => this.setState({devicesFound: []})}
+              style={styles.startLoggingButtonTouchable}>
+              <Text style={styles.startLoggingButtonText}>Clear Devices</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </SafeAreaView>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   body: {
-    height: "100%",
+    height: '100%',
   },
   sectionContainerFlex: {
     flex: 1,
@@ -246,7 +277,7 @@ const styles = StyleSheet.create({
     fontSize: 24,
     marginBottom: 8,
     fontWeight: '600',
-    textAlign: 'center'
+    textAlign: 'center',
   },
   sectionDescription: {
     fontSize: 18,
@@ -287,13 +318,13 @@ const styles = StyleSheet.create({
     color: '#ffffff',
   },
   listPastConnections: {
-      width: "80%",
-      height: 200
+    width: '80%',
+    height: 200,
   },
   itemPastConnections: {
-      padding: 3,
-      fontSize: 18,
-      fontWeight: '400',
+    padding: 3,
+    fontSize: 18,
+    fontWeight: '400',
   },
 });
 
